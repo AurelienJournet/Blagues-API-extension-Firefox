ed59a5a345b827eac276584b2bbffc3eca3a450b
Aurélien JOURNET <aureljournet@orange.fr>
2022-10-17 20:24:08 +0200

    Modification des permissions au strict minimum et modifications mineures

--- background-script.js
+++ background-script.js
@@ -66,7 +66,6 @@ function changerStatutExtension(state) {
   }
 }
 
-// TODO : passer potenitellement en pas async
 function blague() {
   token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMTAyOTcwMTAyMTE2MjgxMTQyNCIsImxpbWl0IjoxMDAsImtleSI6InlGWkc4VldXSGQ3NVZBRjY4VWFLRVhnZmI2S1VXMDJ2VEVnWkJSaGZYSlVvZWNjSWhEIiwiY3JlYXRlZF9hdCI6IjIwMjItMTAtMTJUMTA6MjU6MjMrMDA6MDAiLCJpYXQiOjE2NjU1NzAzMjN9.eUvOmnhenNh_6JbI51ZR4Qa6oLT-kOhjy36hp0DKGM0";
   url = "https://www.blagues-api.fr/api/type/" + typeBlagueBackground + "/random";
@@ -92,37 +91,25 @@ function blague() {
   }
 }
 
-
 async function recevoirMessages(message) {
 
-
-
   if (message.typeBlaguePopup) {
-    console.log('message reçu : typeBlaguePopup')
     changerTypeDeBlagueBackground(message.typeBlaguePopup)
   }
   else if (message.statutExtension) {
-    console.log('message reçu : statutExtension')
     changerStatutExtension(message.statutExtension)
   }
   else if (message.demandeTypeBlague) {
-    console.log('message reçu : demandeTypeBlague')
     return Promise.resolve({ response: typeBlagueBackground });
   }
   else if (message.demandeStatutExtension) {
-    console.log('message reçu : demandeStatutExtension')
     return Promise.resolve({ response: modeBlagueActif });
   }
   else if (message.demandeBlague) {
-    console.log('message reçu : demandeBlague')
     if (!blagueEnCours){
-      console.log('blague lancée')
       blague();
     }
   }
 }
-
-
-// TODO : tester avec plusieurs listeners
 
 browser.runtime.onMessage.addListener(recevoirMessages);
--- script.js
+++ script.js
@@ -1,23 +1,15 @@
 var blagueEnCours = false
 var typeBlagueContent = ""
 var statutExtensionActive = false
-
 var minIntervalleEntreDemandesBlagues = 5000
 var horaireDerniereBlague = Date.now() - minIntervalleEntreDemandesBlagues
 
 
-
 function demandeBlagueAuBackground(){
-
-
   if (Date.now() - horaireDerniereBlague > minIntervalleEntreDemandesBlagues){
     browser.runtime.sendMessage({ "demandeBlague": 1 });
     horaireDerniereBlague = Date.now()
   }
-  
-
-
-
 }
 
 document.addEventListener("click", demandeBlagueAuBackground)
